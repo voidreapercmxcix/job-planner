d907b036bffb6ebcbdfd08fddf1aa39596285359
voidreapercmxcix <void.reaper.cmxcix@gmail.com>
2026-09-20 14:40:11 +0100

    Cache v11 for 0.9.2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'jobs-v10';
+const CACHE = 'jobs-v11';
 const FILES = ['./', './index.html', './manifest.json', './icon.svg'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
